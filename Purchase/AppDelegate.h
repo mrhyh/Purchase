a55d50e875f5bae710daf966bb0c8fc0ad5fdd33
叶星龙 <616380056@qq.com>
2015-05-27 13:09:39 +0800

    Initial Commit

---
+++ Purchase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Purchase
+//
+//  Created by 叶星龙 on 15/5/27.
+//  Copyright (c) 2015年 叶星龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
